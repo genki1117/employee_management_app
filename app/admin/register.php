489f76b3a61250bf7772d1c347fcbac5579aa76d
genki1117 <88384341+genki1117@users.noreply.github.com>
2022-05-17 00:21:30 +0900

    commit

--- app/admin/register.php
+++ app/admin/register.php
@@ -8,13 +8,16 @@ echo 'test1';
 if (is_login()) {
     echo 'test2';
     $csrf_token = generate_csrf_token();
+    echo 'test3';
 
     //プルダウン取得
     $pdo = new_PDO();
+    echo 'test4';
     $department_dao = new departmentDao($pdo);
+    echo 'test5';
     $departments = $department_dao->selectAll();
 
-    echo 'test3';
+    echo 'test6';
 
     require __DIR__ . '/../../views/admin/register_view.php';
 
