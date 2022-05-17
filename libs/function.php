7fddb56b8b093632159bb81a983d42a6c3906e58
genki1117 <88384341+genki1117@users.noreply.github.com>
2022-05-17 16:20:52 +0900

    commit

--- libs/function.php
+++ libs/function.php
@@ -20,14 +20,14 @@ function h($str)
 function new_PDO()
 {
     //ローカル
-    // $dsn = "mysql:dbname=employee_management;host=localhost;charset=utf8mb4";
-    // $username = "root";
-    // $password = "root";
+    $dsn = "mysql:dbname=employee_management;host=localhost;charset=utf8mb4";
+    $username = "root";
+    $password = "root";
 
     //heroku
-    $dsn = "mysql:dbname=heroku_4d1209dbba343a9;host=us-cdbr-east-05.cleardb.net;charset=utf8mb4";
-    $username = "b76c9a73f58cc4";
-    $password = "c5ab235f";
+    // $dsn = "mysql:dbname=heroku_4d1209dbba343a9;host=us-cdbr-east-05.cleardb.net;charset=utf8mb4";
+    // $username = "b76c9a73f58cc4";
+    // $password = "c5ab235f";
 
     $options = [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
